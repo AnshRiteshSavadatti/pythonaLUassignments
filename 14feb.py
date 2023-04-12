33ea1a4895da94c66579a7d0437f49c5b44ddf21
Ansh <anshsavadatti@gmail.com>
2023-04-12 14:32:47 +0530

    initial commit

---
+++ 14feb.py
@@ -0,0 +1,40 @@
+a = True
+print(a)
+print(type(a))
+
+b = 2
+print(b)
+print(type(b))
+
+c = 56.56
+print(c)
+print(type(c))
+
+d = 4 + 5j
+print(d)
+print(type(d))
+
+e = "python"
+print(e)
+print(type(e))
+
+f = 2 + 3
+print(f)
+
+g = "Hi " + "hello"
+print(g)
+
+h = 2 * 6
+print(h)
+
+i = "hi " *10
+print(i)
+
+j = 4 / 2 
+print(j)
+
+k = 5.3 // 2 
+print(k)
+
+l = 2**3
+print(l)
